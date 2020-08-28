74afb45e6937cda712370532f774174e26363862
KanishkKalyani <you@example.comkanishkkalyani@gmail.com>
2020-08-28 20:29:08 +0530

    Timer Functionality complete

--- src/components/Countdown.jsx
+++ src/components/Countdown.jsx
@@ -1,19 +1,34 @@
-import React from 'react';
-import Clock from './Clock';
-import CountdownForm from './CountdownForm';
+import React from "react";
+import Clock from "./Clock";
+import CountdownForm from "./CountdownForm";
 
 class Countdown extends React.Component {
-  
+	constructor(props) {
+		super(props);
+		this.state = {
+			count: 0,
+		};
+	}
 
-    render() {
+	resetCount = () => {
+		this.setState({ count: 0 });
+	};
 
-        return (
-            <div>
-                <Clock timeInSeconds={count}/>
-                <CountdownForm onSetCountdownTime={}/>
-            </div>
-        );
-    }
+	render() {
+		const { count } = this.state;
+		return (
+			<div>
+				{count !== 0 && (
+					<Clock timeInSeconds={count} resetCount={this.resetCount} />
+				)}
+				<CountdownForm
+					onSetCountdownTime={val =>
+						this.state.count === 0 && this.setState({ count: val })
+					}
+				/>
+			</div>
+		);
+	}
 }
 
 export default Countdown;
